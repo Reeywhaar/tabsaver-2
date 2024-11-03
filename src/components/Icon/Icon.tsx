40777d54d17c04d771874e751090b7be2492799c
Vyrtsev Mikhail <reeywhaar@gmail.com>
2024-11-04 03:48:57 +0400

    Update icon style to be inline block

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,7 +2,12 @@ import React, { FunctionComponent } from 'react'
 import { IconName, mappings } from './icons'
 
 import classes from './Icon.module.scss'
+import classNames from 'classnames'
 
-export const Icon: FunctionComponent<{ name: IconName } & React.HTMLAttributes<HTMLSpanElement>> = ({ name }) => {
-  return <span className={classes.icon}>{String.fromCharCode(mappings[name])}</span>
+export const Icon: FunctionComponent<{ name: IconName } & React.HTMLAttributes<HTMLSpanElement>> = ({ name, className, ...props }) => {
+  return (
+    <span className={classNames(className, classes.icon)} {...props}>
+      {String.fromCharCode(mappings[name])}
+    </span>
+  )
 }
