3c45b2942bc1a9fa99071bd06b5aa85a61f7ba95
Vyrtsev Mikhail <reeywhaar@gmail.com>
2024-11-26 05:13:51 +0400

    Show unknown label for unknown cookie store ids

--- src/components/ContainerLabel/ContainerLabel.tsx
+++ src/components/ContainerLabel/ContainerLabel.tsx
@@ -17,14 +17,31 @@ export const ContainerLabel: FunctionComponent<{ id?: string }> = ({ id }) => {
     return ctx
   })
 
+  const cntData = useMemo(() => {
+    if (cntResource.error) {
+      return {
+        name: 'Unknown',
+        colorCode: '#dddddd',
+      }
+    }
+    if (cntResource.value) {
+      return {
+        name: cntResource.value.name,
+        colorCode: cntResource.value.colorCode,
+      }
+    }
+
+    return null
+  }, [cntResource.value, cntResource.error])
+
   const textColor = useMemo(() => {
-    if (!cntResource.value?.colorCode) return undefined
-    const rgb = (
-      [cntResource.value.colorCode.substring(1, 3), cntResource.value.colorCode.substring(3, 5), cntResource.value.colorCode.substring(5, 7)] as const
-    ).map(v => parseInt(v, 16)) as [number, number, number]
+    if (!cntData?.colorCode) return undefined
+    const rgb = ([cntData.colorCode.substring(1, 3), cntData.colorCode.substring(3, 5), cntData.colorCode.substring(5, 7)] as const).map(v =>
+      parseInt(v, 16)
+    ) as [number, number, number]
     const hsl = rgbToHsl(...rgb)
     return hsl[2] > 0.5 ? '#000' : '#fff'
-  }, [cntResource.value?.colorCode])
+  }, [cntData?.colorCode])
 
   useEffect(() => {
     cntResource.task.call()
@@ -32,11 +49,11 @@ export const ContainerLabel: FunctionComponent<{ id?: string }> = ({ id }) => {
 
   if (!id) return null
   if (id === DEFAULT_COOKIE_STORE_ID) return null
-  if (!cntResource.value) return null
+  if (!cntData) return null
 
   return (
-    <div className={classes.cnt} style={{ '--bg-color': cntResource.value.colorCode, '--text-color': textColor } as CSSProperties}>
-      {cntResource.value.name}
+    <div className={classes.cnt} style={{ '--bg-color': cntData.colorCode, '--text-color': textColor } as CSSProperties}>
+      {cntData.name}
     </div>
   )
 }
