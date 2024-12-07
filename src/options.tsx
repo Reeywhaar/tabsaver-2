ee6d456c7c0b9c6a410fda9f8450cb69638551a5
Vyrtsev Mikhail <reeywhaar@gmail.com>
2024-12-08 02:51:50 +0400

    Convert styling to css variables and add dark mode

--- src/options.tsx
+++ src/options.tsx
@@ -4,6 +4,8 @@ import { Options } from './components/Options/Options'
 import { DataProvider } from './components/DataProvider'
 import { NotificationProvider } from './components/Notifications/NotificationsContext'
 
+import '@app/styles/colors.css'
+
 function main() {
   const root = createRoot(document.getElementsByClassName('main')[0])
   root.render(
--- src/panel.tsx
+++ src/panel.tsx
@@ -2,6 +2,8 @@ import { App } from '@app/components/App/App'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
+import '@app/styles/colors.css'
+
 function main() {
   const root = createRoot(document.getElementsByClassName('main')[0])
   root.render(<App />)
